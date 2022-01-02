e32f636d4bd55161d2dbf6b0a51a8bef57c5ab5b
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-02 23:02:00 +0000

    #   Another method of generating secure password.

--- main.py
+++ main.py
@@ -9,18 +9,51 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-pwd_character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~0123456789'
-pwd = ''
+
+
+# pwd_character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~0123456789'
+# pwd = ''
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def gen_pwd():
-    global pwd
-    pwd = ''
+    # global pwd
+    # pwd = ''
+    # pwd_entry.delete(0, END)
+    # while len(pwd) < 15:
+    #     pwd = pwd + random.choice(pwd_character)
+    # pwd_entry.insert(END, pwd)
+
+    #   Another method of generating secure password.
     pwd_entry.delete(0, END)
-    while len(pwd) < 15:
-        pwd = pwd + random.choice(pwd_character)
-    pwd_entry.insert(END, pwd)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    pwd_entry.insert(END, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
